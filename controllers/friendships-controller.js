a79023bbc4c4f4562b15b3e91ad93ea6190003d4
Sara Chang <sara@Avocado.lan>
2024-05-29 15:44:01 -0700

    rename user profile names and make public folder accessible

--- controllers/friendships-controller.js
+++ controllers/friendships-controller.js
@@ -1,5 +1,19 @@
+const fs = require("fs");
+
+function loadUsers() {
+  const users = JSON.parse(fs.readFileSync("./data/users.json"));
+  return users;
+}
+
 const friendshipDetails = async (_req, res) => {
-  res.send("this is the friendships route, showing friendship details 👯");
+  const users = loadUsers();
+
+  res.send(users[1]["profile-photo"]);
+  // res.json({
+  //   message: "this is the friendships route, showing friendship details 👯",
+  //   user: users,
+  //   image: users[1]["profile-photo"],
+  // });
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const friendshipsRoutes = require("./routes/friendships-routes");
 require("dotenv").config();
 const { PORT, BACKEND_URL } = process.env;
 
+app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 app.use("/friendships", friendshipsRoutes);
