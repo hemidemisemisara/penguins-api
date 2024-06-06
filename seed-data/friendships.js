495c72a1dc93e12da389cad59930adc8253f52d4
Sara Chang <sara@Avocado.lan>
2024-06-05 19:28:34 -0700

    add seed-data folder and all data files

---
+++ seed-data/friendships.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: "27d0e85e-f594-4f6e-9bd9-c9bd894f53c9",
+    "user-one": "35e01b22-87fa-4ed5-a681-a92736878549",
+    "user-two": "d91d81a2-7887-42d7-a73a-6a9e63a22e68",
+    "friends-since": "Apr 2024",
+  },
+  {
+    id: "76c6ca86-92fd-42ff-b303-47e9272c894f",
+    "user-one": "35e01b22-87fa-4ed5-a681-a92736878549",
+    "user-two": "419573ec-d276-4833-b1dd-399ef5c812b2",
+    "friends-since": "Oct 2018",
+  },
+];
---
+++ seed-data/users.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    id: "35e01b22-87fa-4ed5-a681-a92736878549",
+    "first-name": "Sara",
+    "last-name": "Chang",
+    email: "hemidemisemisara@gmail.com",
+    "profile-photo": "http://localhost:8080/images/user-profiles/user-1.png",
+    location: "Vancouver, Canada",
+    timezone: "PST",
+  },
+  {
+    id: "d91d81a2-7887-42d7-a73a-6a9e63a22e68",
+    "first-name": "Amanda",
+    "last-name": "Ourofino",
+    email: "amanda@example.com",
+    "profile-photo": "http://localhost:8080/images/user-profiles/user-2.png",
+    location: "Brasilia, Brazil",
+    timezone: "BRT",
+  },
+];
