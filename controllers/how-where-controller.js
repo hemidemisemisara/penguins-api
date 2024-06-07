455b131dfd7870525e77db682ba0bc18b301bacf
Sara Chang <sara@Avocado.lan>
2024-06-07 14:03:00 -0700

    fix bug - if the put method doesn't have an image still updating the database

--- controllers/how-where-controller.js
+++ controllers/how-where-controller.js
@@ -1,23 +1,28 @@
 const knex = require("knex")(require("../knexfile"));
-// const path = require("path");
 
 const howWhereUpdate = async (req, res) => {
   try {
     const howWhereID = req.params.id;
     const { title, description } = req.body;
-    const image = req.file;
-    const imageFileName = image.filename;
-    console.log("image", image);
-    console.log("imageFileName", imageFileName);
-    const imageUrl = `http://localhost:8080/images/how-where/${imageFileName}`;
-    const data = await knex("how-where").where({ id: howWhereID }).update({
-      image: imageUrl,
+
+    let updateData = {
       description: description,
       "image-title": title,
-    });
+    };
+
+    if (req.file) {
+      const imageFileName = req.file.filename;
+      const imageUrl = `http://localhost:8080/images/how-where/${imageFileName}`;
+      updateData.image = imageUrl;
+    }
+
+    const data = await knex("how-where")
+      .where({ id: howWhereID })
+      .update(updateData);
     console.log("data:", data);
+
     res.status(200).json({
-      message: "Updated how & where succesfully",
+      message: "Updated how & where successfully",
     });
   } catch (error) {
     console.error(error);
--- routes/how-where-routes.js
+++ routes/how-where-routes.js
@@ -3,22 +3,19 @@ const howWhereController = require("../controllers/how-where-controller");
 const upload = require("../config/multerConfig");
 
 router.put("/:id", async (req, res) => {
-  // use timestamp for the image file name
-  let timestamp = Date.now();
-  //   upload photo to the public folder
-  await upload("how-where", timestamp).single("image")(
-    req,
-    res,
-    async (err) => {
-      // wait until the upload is completed, and if there is any error
+  try {
+    const timestamp = Date.now();
+    upload("how-where", timestamp).single("image")(req, res, async (err) => {
       if (err) {
         console.error(err);
         return res.status(500).json({ message: "Error uploading file" });
       }
-      // move on to the controllerand update database's data
       await howWhereController.howWhereUpdate(req, res);
-    }
-  );
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error updating how & where we met" });
+  }
 });
 
 module.exports = router;
